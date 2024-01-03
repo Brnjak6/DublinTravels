3898d9a798d5fc0a64681b9812691df7e613d2b6
OPTIC <68736438+Brnjak6@users.noreply.github.com>
2024-01-03 11:41:13 +0000

    CORS corrections

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,6 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({ origin: 'https://dt-frontend.onrender.com' }));
-app.use(cors({ origin: 'http://localhost:3000' }));
 
 //middleware
 //parsing raw data to JSON instantly
